05b38342deb4c6fcf8ec97d49ed43f83a00aefee
Samuel Duarte <sduartegit@gmail.com>
2025-02-20 11:32:38 -0300

    First commit

---
+++ src/main/java/com/duartetech/api_consultas/ApiConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.duartetech.api_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duartetech/api_consultas/ApiConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duartetech.api_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
